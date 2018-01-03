f11852cdf633c69106f11999384b705606503685
gpbenton <github.gpbenton@xoxy.net>
2018-01-03 19:45:49 +0000

    add backup script

---
+++ backup_nextcloud.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# backup volumes to my mega.nz account
+
+docker pause nextcloud_app_1
+docker run -v nextcloud_db:/volume -v /tmp:/backup --rm loomchild/volume-backup backup nextcloud_db_$(date +%Y%m%d)
+docker pause caddy
+docker run -v nextcloud_data_dir:/volume -v /tmp:/backup --rm loomchild/volume-backup backup nextcloud_data_dir_$(date +%Y%m%d)
+docker unpause caddy
+docker unpause nextcloud_app_1
+megaput --no-progress --path=/Root/slate-500 /tmp/nextcloud_db_$(date +%Y%m%d).tar.bz2
+if [ $? -ne 0 ] 
+then
+    echo "megaput failed for nextcloud_db"
+else
+    echo "megaput succeeded"
+    # Can't clean up as file owned by root
+    #rm /tmp/nextcloud_db_$(date +%Y%m%d).tar.bz2
+fi
+megaput --no-progress --path=/Root/slate-500 /tmp/nextcloud_data_dir_$(date +%Y%m%d).tar.bz2
+if [ $? -ne 0 ] 
+then
+    echo "megaput failed for nextcloud_data_dir"
+else
+    echo "megaput succeeded"
+    # Can't clean up as file owned by root
+    # rm /tmp/nextcloud_data_dir_$(date +%Y%m%d).tar.bz2
+fi
