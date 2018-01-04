6370732bcc3a7894bd66b64e9b592fafb0abf3c4
gpbenton <github.gpbenton@xoxy.net>
2018-01-04 11:55:19 +0000

    Use pg_dump to back up db

--- backup_nextcloud.sh
+++ backup_nextcloud.sh
@@ -3,17 +3,17 @@
 # backup volumes to my mega.nz account
 
 docker pause nextcloud_app_1
-docker run -v nextcloud_db:/volume -v /tmp:/backup --rm loomchild/volume-backup backup nextcloud_db_$(date +%Y%m%d)
+docker exec nextcloud_db_1 pg_dump --oids --username=nextcloud nextcloud |bzip2 > /tmp/nextcloud_db_$(date +%Y%m%d).txt.bz2
 docker run -v nextcloud_data_dir:/volume -v /tmp:/backup --rm loomchild/volume-backup backup nextcloud_data_dir_$(date +%Y%m%d)
 docker unpause nextcloud_app_1
-megaput --no-progress --path=/Root/$(uname -n) /tmp/nextcloud_db_$(date +%Y%m%d).tar.bz2
+megaput --no-progress --path=/Root/$(uname -n) /tmp/nextcloud_db_$(date +%Y%m%d).txt.bz2
 if [ $? -ne 0 ] 
 then
     echo "megaput failed for nextcloud_db"
 else
     echo "megaput succeeded"
     # Can't clean up as file owned by root
-    #rm /tmp/nextcloud_db_$(date +%Y%m%d).tar.bz2
+    #rm /tmp/nextcloud_db_$(date +%Y%m%d).txt.bz2
 fi
 megaput --no-progress --path=/Root/$(uname -n) /tmp/nextcloud_data_dir_$(date +%Y%m%d).tar.bz2
 if [ $? -ne 0 ] 
