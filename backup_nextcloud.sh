b2510439e07923cf4b8caffb64c48fbe4500788d
Graham <github.gpbenton@xoxy.net>
2019-09-22 09:49:31 +0100

    Various

--- backup_nextcloud.sh
+++ backup_nextcloud.sh
@@ -6,5 +6,5 @@ BACKUP_DIR=${HOME}/nextcloud/backup
 
 docker pause nextcloud_app_1
 docker exec nextcloud_db_1 pg_dump --username=nextcloud nextcloud |bzip2 > ${BACKUP_DIR}/nextcloud_db_backup.txt.bz2
-docker run -v nextcloud_data_dir:/volume -v /tmp:/backup --rm loomchild/volume-backup backup ${BACKUP_DIR}/nextcloud_data_dir_backup
+docker run -v nextcloud_data_dir:/volume -v ${BACKUP_DIR}:/backup --rm loomchild/volume-backup backup nextcloud_data_dir_backup
 docker unpause nextcloud_app_1
